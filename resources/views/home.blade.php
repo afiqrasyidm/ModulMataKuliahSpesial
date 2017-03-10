d7f6b8466ff84f57f6fbb966a2603b305ea9995d
fauzandiMB <fauzandib@gmail.com>
2017-03-10 14:42:45 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
+        <script type="text/javascript" src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}" />
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+       
+    </body>
+</html>
