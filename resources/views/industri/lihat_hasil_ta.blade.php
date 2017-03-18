04879cbd6969875b5d0f3e19c44ed339de2f0366
monicagustin <moniciagustin13@gmail.com>
2017-03-18 18:16:09 +0700

    fixed untracked files

---
+++ resources/views/industri/lihat_hasil_ta.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.layout_industri')
+
+@section('title','Home Page')
+
+@section('mainContent')
+<div class="col-sm-10">
+
+<div class="box-body table-responsive no-padding">
+								 <table class="table table-hover">
+									 <tr>
+										 <th>Mahasiswa</th>
+										 <th>Judul</th>
+										 <th>Status</th>
+
+									 </tr>
+									 <tr>
+										 <td>Monica</td>
+										 <td>ABCDEFG</td>
+										 <td><span class="label label-success">Approved</span></td>
+
+									 </tr>
+									 <tr>
+										 <td>Monica</td>
+										<td>ABCDEFG</td>
+										<td><span class="label label-success">Approved</span></td>
+									</tr>
+									 <tr>
+
+										 <td>Monica</td>
+										<td>ABCDEFG</td>
+										<td><span class="label label-success">Approved</span></td>
+									</tr>
+									 <tr>
+										 <td>Monica</td>
+										<td>ABCDEFG</td>
+										<td><span class="label label-success">Approved</span></td>
+									 </tr>
+								 </table>
+							 </div><!-- /.box-body -->
+						 </div>
+
+@endsection
