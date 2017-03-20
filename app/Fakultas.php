7a85679d48c72660b9e967ea92e828a2424b3901
fauzandiMB <fauzandib@gmail.com>
2017-03-20 13:02:45 +0700

    rapihin model

--- app/Fakultas.php
+++ app/Fakultas.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fakultas extends Model
 {
-      protected $table = 'Fakultas';
+    protected $table = 'Fakultas';
     protected $fillable = ['nama_fakultas', ' id_universitas'];
 }
--- app/Prodi.php
+++ app/Prodi.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prodi extends Model
 {
-     protected $table = 'Prodi';
+    protected $table = 'Prodi';
     protected $fillable = ['nama_prodi', 'id_fakultas'];
 }
