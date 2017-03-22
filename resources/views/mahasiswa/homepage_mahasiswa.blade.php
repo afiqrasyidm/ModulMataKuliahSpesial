2345f7f18c991fe944896436122848d22afd0429
Mischelle <mischelle@Mischelles-MacBook-Air.local>
2017-03-22 15:12:43 +0700

    update gambar

--- resources/views/mahasiswa/homepage_mahasiswa.blade.php
+++ resources/views/mahasiswa/homepage_mahasiswa.blade.php
@@ -7,7 +7,22 @@
 <div class="col-md-12">
 	<center>
 		<h1>Alur Pengerjaan Tugas Akhir</h1>
-		<img src="{{ asset('img/flowchart.png') }}">
+
+		 <?php 
+            if($_SESSION["mahasiswa"]->jenjang=="S1"){
+
+              echo "<img src='";
+              echo asset('img/s1.png');
+              echo "' style='height: 400px'/>";
+            }
+
+            else{
+
+              echo "<img src='";
+              echo asset('img/s2.png');
+              echo "' style='height: 400px'/>";
+            }
+          ?>
 	</center>
 </div>
         
