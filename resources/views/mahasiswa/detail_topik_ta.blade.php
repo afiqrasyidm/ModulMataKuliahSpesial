217ebbf6bce9941d0b6db1af48fa1d13bfe9b243
monicagustin <moniciagustin13@gmail.com>
2017-03-27 18:02:42 +0700

    Update halaman detail topik, beberapa masih hardcoded

--- resources/views/mahasiswa/detail_topik_ta.blade.php
+++ resources/views/mahasiswa/detail_topik_ta.blade.php
@@ -11,33 +11,54 @@
 <br><br>
         <section class="content">
 
+						<h4><b>{{$topik->topik_ta}}</b></h4>
+
           <!-- Default box -->
-          <div class="box">
-            <div class="box-header with-border">
-              <h3 class="box-title"> Detail Topik : {{$topik->topik_ta}}</h3>
-              <div class="box-tools pull-right">
-              </div>
-            </div>
-            <div class="box-body">
-              {{$topik->deskripsi}}
-            </div><!-- /.box-body -->
-            <div class="box-footer">
-           
-						
-						@if (isset($industri))
-							Topik ini diajukan oleh industri : {{$industri->nama_industri}}
-							
-						@else
-							Topik ini diajukan oleh dosen :{{$dosen->nama_dosen}}
-							
-						
-						@endif
-					
-		   
-		   
-		   </div><!-- /.box-footer-->
-          </div><!-- /.box -->
-		  
+					      <table class="table table-bordered">
+
+	    <tbody>
+
+
+	          <th width ="20%" bgcolor="#86b7e3">Pengaju</th>
+	            <td bgcolor="#c0c5cc">@if (isset($industri))
+						{{$industri->nama_industri}}
+
+							@else
+							{{$dosen->nama_dosen}}
+
+
+							@endif</td>
+
+	        </tr>
+					<tr>
+					 <th bgcolor="#86b7e3">Diajukan Pada</th>
+						 <td bgcolor="#c0c5cc"> {{$topik->created_at}}</td>
+
+				 </tr>
+				 <tr>
+					<th bgcolor="#86b7e3">Lowongan</th>
+						<td bgcolor="#c0c5cc"> 10 orang</td>
+
+				</tr>
+				 <tr>
+					 <th bgcolor="#86b7e3">Pendaftar</th>
+						 <td bgcolor="#c0c5cc"> 9 orang</td>
+
+				 </tr>
+
+
+	        <tr>
+	          <th bgcolor="#86b7e3">Deskripsi</th>
+	            <td bgcolor="#c0c5cc"> {{$topik->deskripsi}}</td>
+
+	        </tr>
+
+
+
+
+	    </tbody>
+	</table>
+
 		<center>
 			<a href="/mahasiswa/pengajuan-topik-ta-dosen-industri/{{$topik->id_topik}}">
 				<button class="btn btn-primary">Ambil Topik Ini</button>
