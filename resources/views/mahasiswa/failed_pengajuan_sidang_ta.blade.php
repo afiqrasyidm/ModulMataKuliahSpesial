a503693d56fcc57d065d89a0b2a0a471b015cc79
Aneira Dwira <aneiradwira@Aneiras-MacBook-Pro.local>
2017-04-13 21:28:46 +0700

    update tampilan

--- resources/views/mahasiswa/failed_pengajuan_sidang_ta.blade.php
+++ resources/views/mahasiswa/failed_pengajuan_sidang_ta.blade.php
@@ -3,7 +3,7 @@
 @section('title','Pengajuan Sidang TA')
 
 @section('titleMainContent')
-Anda Belum Dapat Melakukan Pengajuan Sidang TA
+Anda Belum Dapat Melakukan Pengajuan Sidang
 
 @endsection
 
@@ -15,11 +15,10 @@ Anda Belum Dapat Melakukan Pengajuan Sidang TA
 
 
 if($tugas_akhir == null){
-	echo "<div class='col-md-3'></div>";
-	echo "<div  class='col-md-9'>";
-	echo "<div style='margin-top:225px;''>";
-	echo "<h2 style='color:red;'>Harap mengambil Tugas Akhir terlebih dahulu!</h2>";
-	echo "<p>Anda tidak dapat melakukan pengajuan permohonan sidang tugas akhir sebelum mengambil tugas akhir</p>";
+	
+	echo "<b>Harap mengambil Tugas Akhir terlebih dahulu!</b>";
+	echo " Anda tidak dapat melakukan pengajuan permohonan sidang tugas akhir sebelum mengambil tugas akhir";
+	echo "<br><br>";
  	echo "<p>Silahkan mengambil tugas akhir";
  	echo "<a href='";
  	echo route('mahasiswa/pengajuan-permohonan-ta');
@@ -29,11 +28,8 @@ if($tugas_akhir == null){
 }
 
 else if($tugas_akhir->status_tugas_akhir!=6){
-	echo "<div class='col-md-3'></div>";
-	echo "<div  class='col-md-9'>";
-	echo "<div style='margin-top:225px;''>";
-	echo "<h2 style='color:red;'>Harap menyelesaikan Bimbingan Tugas Akhir terlebih dahulu!</h2>";
-	echo "<p>Anda tidak diperkenankan melakukan pengajuan permohonan sidang tugas akhir sebelum menyelesaikan bimbingan tugas akhir</p>";
+	echo "<b>Harap menyelesaikan Bimbingan Tugas Akhir terlebih dahulu!</b>";
+	echo " Anda tidak diperkenankan melakukan pengajuan permohonan sidang tugas akhir sebelum menyelesaikan bimbingan tugas akhir";
  	//echo "<p>Silahkan mengajukan bimbingan TA <a href="{{route('mahasiswa/pengajuan-permohonan-ta')}}">disini</a></p>";
 }
 ?>
--- resources/views/mahasiswa/failed_upload_hasil_ta.blade.php
+++ resources/views/mahasiswa/failed_upload_hasil_ta.blade.php
@@ -15,11 +15,9 @@ Anda Belum Dapat Mengupload Dokumen TA
 
 
 if($tugas_akhir == null){
-	echo "<div class='col-md-3'></div>";
-	echo "<div  class='col-md-9'>";
-	echo "<div style='margin-top:225px;''>";
-	echo "<h2 style='color:red;'>Harap mengambil Tugas Akhir terlebih dahulu!</h2>";
-	echo "<p>Anda tidak dapat melakukan pengajuan permohonan sidang tugas akhir sebelum mengambil tugas akhir</p>";
+	echo "<b>Harap ambil tugas akhir terlebih dahulu!</b>";
+	echo " Anda tidak dapat melakukan pengajuan permohonan sidang tugas akhir sebelum mengambil tugas akhir";
+	echo "<br><br>";
  	echo "<p>Silahkan mengambil tugas akhir";
  	echo "<a href='";
  	echo route('mahasiswa/pengajuan-permohonan-ta');
@@ -30,17 +28,15 @@ if($tugas_akhir == null){
 
 
 else if($tugas_akhir->status_tugas_akhir!=6){
-	echo "<div class='col-md-3'></div>";
-	echo "<div  class='col-md-9'>";
-	echo "<div style='margin-top:225px;''>";
-	echo "<h2 style='color:red;'>Harap menyelesaikan proses pembuatan tugas akhir terlebih dahulu!</h2>";
-	echo "<p>Anda tidak diperkenankan melakukan upload dokumen sebelum menyelesaikan bimbingan tugas akhir dan diizinkan sidang oleh dosen pembimbing </p>";
+	echo "<b>Harap menyelesaikan proses pembuatan tugas akhir terlebih dahulu!</b>";
+	echo " Anda tidak diperkenankan melakukan upload dokumen sebelum menyelesaikan bimbingan tugas akhir dan diizinkan sidang oleh dosen pembimbing";
 }
 
 else if($pengajuan_sidang == null){
 	echo "<b>Harap melakukan pengajuan sidang terlebih dahulu!</b>";
-	echo "<p>Anda tidak dapat melakukan upload dokumen sebelum melakukan pengajuan sidang tugas akhir</p>";
- 	echo "<p>Silahkan makelukan pengajuan sidang tugas akhir";
+	echo " Anda tidak dapat melakukan upload dokumen sebelum melakukan pengajuan sidang tugas akhir";
+	echo "<br><br>";
+ 	echo "<p>Silahkan melakukan pengajuan sidang tugas akhir";
  	echo "<a href='";
  	echo route('mahasiswa/pengajuan-sidang-ta');
  	echo "'/>";
@@ -51,7 +47,7 @@ else if($pengajuan_sidang == null){
 
 else if($pengajuan_sidang->status!=2){
 	echo "<b>Harap menunggu persetujuan sidang! </b>";
-	echo "Anda tidak diperkenankan melakukan upload dokumen sebelum diizinkan sidang oleh dosen pembimbing";
+	echo " Anda tidak diperkenankan melakukan upload dokumen sebelum diizinkan sidang oleh dosen pembimbing";
 }
 ?>
 
