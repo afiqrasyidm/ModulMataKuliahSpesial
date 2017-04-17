7f33b21420dce5dade4b7057436c5f7b40b42f14
Aneira Dwira <aneiradwira@Aneiras-MacBook-Pro.local>
2017-04-17 20:32:26 +0700

    update view homepage penguji

--- resources/views/dosen/DosenPenguji/home_dosen_penguji.blade.php
+++ resources/views/dosen/DosenPenguji/home_dosen_penguji.blade.php
@@ -3,13 +3,29 @@
 @section('title','Home Page')
 
 @section('mainContent')
-<div class="col-md-12">
-	<center>
-		<h1>Alur Tugas Akhir</h1><br><br>
-		<img src="{{ asset('img/penguji.png') }}" style="width: 50% ; height: 30%">
-	</center>
-</div>
+<section class="content">
+<div class="center-form">
+<div class=".col-md-11">
 
+
+ <div class="box box-primary">
+        <div class="box-header with-border">
+            <center><b><h2 class="title-index">Alur Pengerjaan Tugas Akhir</h2></b></center>
+          </div><!-- /.box-header -->
+          <div class="box-body">
+          <center><br>
+		<img src="{{ asset('img/penguji.png') }}" style="width: 65% ; height: 65%"><br><br>
+          </center>
+          </div>
+
+
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+              </div>
+            </div><!--/.col (right) -->
+
+</section>
+        
 
         
 @endsection
