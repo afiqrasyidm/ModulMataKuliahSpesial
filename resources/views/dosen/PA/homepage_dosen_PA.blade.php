5058cb01fb3fa2f179f97e17ee7f9c2e0cf9ceb5
Aneira Dwira <aneiradwira@Aneiras-MacBook-Pro.local>
2017-04-17 20:23:00 +0700

    update view homepage PA

--- resources/views/dosen/PA/homepage_dosen_PA.blade.php
+++ resources/views/dosen/PA/homepage_dosen_PA.blade.php
@@ -4,11 +4,28 @@
 
 @section('mainContent')
 
-<div class="col-md-12">
-	<center>
-		<h1>Alur Tugas Akhir</h1><br><br>
-		<img src="{{ asset('img/PA.png') }}" style="width: 50% ; height: 30%">
-	</center>
-</div>
+
+<section class="content">
+<div class="center-form">
+<div class=".col-md-11">
+
+
+ <div class="box box-primary">
+        <div class="box-header with-border">
+            <center><b><h2 class="title-index">Alur Pengerjaan Tugas Akhir</h2></b></center>
+          </div><!-- /.box-header -->
+          <div class="box-body">
+          <center><br>
+		<img src="{{ asset('img/PA.png') }}" style="width: 50% ; height: 50%">
+            </center>
+          </div>
+
+<br><br>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+              </div>
+            </div><!--/.col (right) -->
+
+</section>
         
 @endsection
