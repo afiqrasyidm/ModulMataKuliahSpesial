7450eba1fad31ee41e2d5ac93118fb10f65848ae
aneira.dwira <aneira.dwira@gmail.com>
2017-04-23 12:20:53 +0700

    update verif sba

--- resources/views/staf/form_verifikasi_sidang_ta.blade.php
+++ resources/views/staf/form_verifikasi_sidang_ta.blade.php
@@ -24,58 +24,63 @@
     <div class="box-body">
      <br>
         <form class="form-horizontal" method="post" action="">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 
-              <table class="table table-bordered">
+            <div class="form-group">
+                <label class="control-label col-sm-3">Nama Mahasiswa :</label>
+                <div class="col-sm-6">
+                     <input class="form-control" value="{{$ta->nama_mahasiswa}}" disabled>
+                </div>
+            </div>
+               
 
-              <tbody>
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Nama Mahasiswa</th>
-                <td>{{$ta->nama_mahasiswa}}</td>
-                </tr>
+             <div class="form-group">
+                <label class="control-label col-sm-3">NPM :</label>
+                <div class="col-sm-6">
+                    <input class="form-control" value="{{$ta->NPM}}" disabled>
+                </div>
+            </div>
 
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">NPM</th>
-                <td>{{$ta->NPM}}</td>
-                </tr>
+             <div class="form-group">
+                <label class="control-label col-sm-3">Judul TA :</label>
+                <div class="col-sm-6">
+                    <input class="form-control" value="{{$ta->judul_ta}}" disabled></div>
+             </div>
 
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Judul TA</th>
-                <td>{{$ta->judul_ta}}</td>
-                </tr>
-
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Topik</th>
-                <td>{{$ta->topik_ta}}</td>
-                </tr>
+             <div class="form-group">
+                <label class="control-label col-sm-3">Topik :</label>
+                <div class="col-sm-6">
+                    <input class="form-control" value="{{$ta->topik_ta}}" disabled>
+                </div>
+             </div>
 
 
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Dosen Pembimbing</th>
-                <td>
-				Mischelle
-				</td>
-                </tr>
+            <div class="form-group">
+                <label class="control-label col-sm-3">Dosen Pembimbing :</label>
+                <div class="col-sm-6">
+                    <input class="form-control" value="Mischelle" disabled>
+                </div>
+            </div>
 
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Dosen Penguji 1</th>
-                <td>
-						<select name="dosen_penguji_1">
-							<option value="0">Pilih Nama Dosen</option>
-							  @foreach ($dosen as $dosen)
+             <div class="form-group">
+                <label class="control-label col-sm-3">Dosen Penguji :</label>
+                <div class="col-sm-6">
+					<select name="dosen_penguji_1" class="form-control select2" style="width: 100%;">
+						<option value="0">Pilih Nama Dosen</option>
+							@foreach ($dosen as $dosen)
 						
-									<option value="{{$dosen->id_dosen}}">{{$dosen->nama_dosen}}</option>
+						<option value="{{$dosen->id_dosen}}">{{$dosen->nama_dosen}}</option>
 							
 							 @endforeach
-						</select>
-				</td>
-                </tr>
+					</select>
+                </div>
+            </div>
 
-				 <tr>
-                <th width ="20%" bgcolor="#86b7e3">Dosen Penguji 2</th>
-                <td>
-						<select name="dosen_penguji_2">
+			 <div class="form-group">
+                <label class="control-label col-sm-3">Dosen Penguji 2</label>
+                <div class="col-sm-6">		
+                    <select name="dosen_penguji_2" class="form-control select2" style="width: 100%;">
 							<option value="0">Pilih Nama Dosen</option>
 							  @foreach ($dosen2 as $dosen)
 						
@@ -83,13 +88,13 @@
 							
 							 @endforeach
 						</select>
-				</td>
-                </tr>
+				</div>
+            </div>
 				
-				 <tr>
-                <th width ="20%" bgcolor="#86b7e3">Dosen Penguji 3</th>
-                <td>
-						<select name="dosen_penguji_3">
+			 <div class="form-group">
+                <label class="control-label col-sm-3">Dosen Penguji 3</label>
+                <div class="col-sm-6">
+						<select name="dosen_penguji_3" class="form-control select2" style="width: 100%;">
 							<option value="0">Pilih Nama Dosen</option>
 							  @foreach ($dosen3 as $dosen)
 						
@@ -98,18 +103,22 @@
 							 @endforeach
 						</select>
 				
-				</td>
-                </tr>
+				</div>
+            </div>
 				
-                <tr>
-                <th width ="20%" bgcolor="#86b7e3">Waktu Sidang</th>
-                <td><input name="waktu_sidang" type="date" id="datepicker" value="<?php echo date ("Y-m-d");?> ">
-                </td>
-				</td>
-                </tr>
-        
-				</tbody>
-			</table>
+            <div class="form-group">
+                <label class="control-label col-sm-3">Waktu Sidang</label>
+                <div class="col-sm-6">
+                   <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                       
+                      <input class="form-control select2" style="width: 100%;" name="waktu_sidang" type="date" id="datepicker" value="<?php echo date ("Y-m-d");?> ">
+                    </div>
+                </div>
+            </div>
+			
             <input name="id_pengajuan" type="text" id="datepicker" value="{{$ta->id_pengajuan}}" hidden>
 <br>
             <center><button  class="btn btn-primary" type="submit">Verified</button></center>
