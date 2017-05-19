a9f80dc83970edec33d59930e9e3092135d708e1
monicagustin <moniciagustin13@gmail.com>
2017-05-19 21:51:00 +0700

    benerin tolak bimbingan

--- app/Http/Controllers/DosenPembimbingController.php
+++ app/Http/Controllers/DosenPembimbingController.php
@@ -252,10 +252,11 @@ function ubah_status_sidangPost($id_tugas_akhir)
 
 	function set_verifikasi_bimbingan($status, $id_dpt) {
 		session_start();
-
+		
 	 	$pembimbing = Dosen_pembimbing::find($id_dpt);
 	 	$pembimbing->status_dosen_pembimbing = $status;
 	 	// $dosen_pembimbing->id_maker = $_SESSION["id_user"];
+
 	 	$pembimbing->save();
 
 	 	if($status == 2) {
@@ -263,10 +264,19 @@ function ubah_status_sidangPost($id_tugas_akhir)
 	 			->where('id_tugas_akhir', '=', $pembimbing->id_tugas_akhir)
 	 			->update(['status_tugas_akhir' => 10]);
 
-	 	} else {
+	 		DB::table('dosen_pembimbing_ta')
+	 		->where('id', '=', $id_dpt)
+	 		->update(['status_dosen_pembimbing' => 2 ]);
+
+	 	} 
+	 	else {
 	 		DB::table('tugas_akhir')
 	 			->where('id_tugas_akhir', '=', $pembimbing->id_tugas_akhir)
 	 			->update(['status_tugas_akhir' => 8]);
+
+	 		DB::table('dosen_pembimbing_ta')
+	 		->where('id', '=', $id_dpt)
+	 		->update(['status_dosen_pembimbing' => 3 ]);
 	 	}
 	 	
 	 	return redirect()->route('dosen/pembimbing/verifikasi-bimbingan');
